84ce88cb7f31246be7a9172f5dc8006bb9430a9a
Roman Portianko <rportyanko10@gmail.com>
2023-03-11 13:04:45 +0200

    Added Firebase and Bootstrap to the project

--- src/index.js
+++ src/index.js
@@ -4,6 +4,26 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore, addDoc, collection } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCv5boKiHH6jXk6Cp491m7oOLPqtkHB7Ww",
+    authDomain: "step-exam.firebaseapp.com",
+    projectId: "step-exam",
+    storageBucket: "step-exam.appspot.com",
+    messagingSenderId: "871581441255",
+    appId: "1:871581441255:web:f6ed273b1b4214ab6ad642"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
